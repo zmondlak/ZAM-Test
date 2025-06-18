4572444ab2a590bc7fcffdf042b501358f238e49
zmondlak <zmondlak@gmail.com>
2025-06-18 16:01:13 -0400

    Fixed

--- ZAM Test.R
+++ ZAM Test.R
@@ -2,12 +2,6 @@
 #Hello World!
 #Test Code
 #Test Code 2
-<<<<<<< HEAD
 #This sentence should only be in master
-=======
-<<<<<<< HEAD
 #This sentence should only be in new-branch-name
-=======
 #This sentence should only be in master
->>>>>>> new-branch-name
->>>>>>> master
